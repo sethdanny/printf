e33864fa67372d04a13c1be4cb112a317941d8c4
sethdanny <naddulidaniel1994@gmail.com>
2022-10-17 10:08:15 +0300

    print integer

--- main.h
+++ main.h
@@ -10,14 +10,40 @@ int _strlen(const char *str);
 int print(char *);
 char *itoa(long int, int);
 
-/* print a character */
-int _putchar(char c);
-
 /* printf.c */
-int _printf(const char *format, ...);
+int _printf(const char *, ...);
 
 /* handler.c */
 int handler(const char *, va_list, int *);
 int percent_handler(const char *, va_list, int *);
+
+/* printers */
+int print_string(va_list);
+int print_char(va_list);
+int print_integer(va_list);
+int print_binary(va_list);
+int print_rot(va_list);
+int print_unsigned(va_list);
+int print_octal(va_list);
+int print_hexadecimal_low(va_list);
+int print_hexadecimal_upp(va_list);
+int print_pointer(va_list);
+int print_rev_string(va_list);
+
+/* _putchar.c */
+int _putchar(char);
+int buffer(char);
+
+/**
+ * struct _format - Typedef struct
+ *
+ * @type: Format
+ * @f: The function associated
+ **/
+typedef struct _format
+{
+	char type;
+	int (*f)(va_list);
+} format;
 
 #endif
